eca5943b4817c5b6b77809007393f6ac9d565d38
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-10-12 12:37:53 +0600

    navbar and sidebar

---
+++ resources/views/backend/partials/sidebar.blade.php
@@ -0,0 +1,219 @@
+<nav class="navbar navbar-vertical navbar-expand-lg" style="display:none;">
+    <div class="collapse navbar-collapse" id="navbarVerticalCollapse">
+        <!-- scrollbar removed-->
+        <div class="navbar-vertical-content">
+            <ul class="navbar-nav flex-column" id="navbarVerticalNav">
+                <li class="nav-item">
+                    <!-- parent pages-->
+                    <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-home"
+                            role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="nv-home">
+                            <div class="d-flex align-items-center">
+                                <div class="dropdown-indicator-icon-wrapper"><span
+                                        class="fas fa-caret-right dropdown-indicator-icon"></span></div><span
+                                    class="nav-link-icon"><span data-feather="pie-chart"></span></span><span
+                                    class="nav-link-text">Home</span><span
+                                    class="fa-solid fa-circle text-info ms-1 new-page-indicator"
+                                    style="font-size: 6px"></span>
+                            </div>
+                        </a>
+                        <div class="parent-wrapper label-1">
+                            <ul class="nav collapse parent show" data-bs-parent="#navbarVerticalCollapse"
+                                id="nv-home">
+                                <li class="collapsed-nav-item-title d-none">Home</li>
+                                <li class="nav-item"><a class="nav-link active" href="index-2.html">
+                                        <div class="d-flex align-items-center"><span class="nav-link-text">E
+                                                commerce</span></div>
+                                    </a><!-- more inner pages-->
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="dashboard/project-management.html">
+                                        <div class="d-flex align-items-center"><span class="nav-link-text">Project
+                                                management</span></div>
+                                    </a><!-- more inner pages-->
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="dashboard/crm.html">
+                                        <div class="d-flex align-items-center"><span class="nav-link-text">CRM</span>
+                                        </div>
+                                    </a><!-- more inner pages-->
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="dashboard/travel-agency.html">
+                                        <div class="d-flex align-items-center"><span class="nav-link-text">Travel
+                                                agency</span><span
+                                                class="badge ms-2 badge badge-phoenix badge-phoenix-warning ">New</span>
+                                        </div>
+                                    </a><!-- more inner pages-->
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="apps/social/feed.html">
+                                        <div class="d-flex align-items-center"><span class="nav-link-text">Social
+                                                feed</span></div>
+                                    </a><!-- more inner pages-->
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <!-- label-->
+                    <p class="navbar-vertical-label">Apps</p>
+                    <hr class="navbar-vertical-line" /><!-- parent pages-->
+                    <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-e-commerce"
+                            role="button" data-bs-toggle="collapse" aria-expanded="false"
+                            aria-controls="nv-e-commerce">
+                            <div class="d-flex align-items-center">
+                                <div class="dropdown-indicator-icon-wrapper"><span
+                                        class="fas fa-caret-right dropdown-indicator-icon"></span></div><span
+                                    class="nav-link-icon"><span data-feather="shopping-cart"></span></span><span
+                                    class="nav-link-text">E commerce</span>
+                            </div>
+                        </a>
+                        <div class="parent-wrapper label-1">
+                            <ul class="nav collapse parent" data-bs-parent="#navbarVerticalCollapse" id="nv-e-commerce">
+                                <li class="collapsed-nav-item-title d-none">E commerce</li>
+                                <li class="nav-item"><a class="nav-link dropdown-indicator" href="#nv-admin"
+                                        data-bs-toggle="collapse" aria-expanded="true" aria-controls="nv-admin">
+                                        <div class="d-flex align-items-center">
+                                            <div class="dropdown-indicator-icon-wrapper"><span
+                                                    class="fas fa-caret-right dropdown-indicator-icon"></span></div>
+                                            <span class="nav-link-text">Admin</span>
+                                        </div>
+                                    </a><!-- more inner pages-->
+                                    <div class="parent-wrapper">
+                                        <ul class="nav collapse parent show" data-bs-parent="#e-commerce"
+                                            id="nv-admin">
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/add-product.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Add product</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/products.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Products</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/customers.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Customers</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/customer-details.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Customer details</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/orders.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Orders</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/order-details.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Order details</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/admin/refund.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Refund</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li class="nav-item"><a class="nav-link dropdown-indicator" href="#nv-customer"
+                                        data-bs-toggle="collapse" aria-expanded="true" aria-controls="nv-customer">
+                                        <div class="d-flex align-items-center">
+                                            <div class="dropdown-indicator-icon-wrapper"><span
+                                                    class="fas fa-caret-right dropdown-indicator-icon"></span></div>
+                                            <span class="nav-link-text">Customer</span>
+                                        </div>
+                                    </a><!-- more inner pages-->
+                                    <div class="parent-wrapper">
+                                        <ul class="nav collapse parent show" data-bs-parent="#e-commerce"
+                                            id="nv-customer">
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/homepage.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Homepage</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/product-details.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Product details</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/products-filter.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Products filter</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/cart.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Cart</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/checkout.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Checkout</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/shipping-info.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Shipping info</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/profile.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Profile</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/favourite-stores.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Favourite stores</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/wishlist.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Wishlist</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/order-tracking.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Order tracking</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                            <li class="nav-item"><a class="nav-link"
+                                                    href="apps/e-commerce/landing/invoice.html">
+                                                    <div class="d-flex align-items-center"><span
+                                                            class="nav-link-text">Invoice</span></div>
+                                                </a><!-- more inner pages-->
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div><!-- parent pages-->
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="navbar-vertical-footer"><button
+            class="btn navbar-vertical-toggle border-0 fw-semibold w-100 white-space-nowrap d-flex align-items-center"><span
+                class="uil uil-left-arrow-to-left fs-8"></span><span
+                class="uil uil-arrow-from-right fs-8"></span><span class="navbar-vertical-footer-text ms-2">Collapsed
+                View</span></button></div>
+</nav>
