a52c79558374ff8a15962acb3f0a01c4d7d8448e
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-10-13 10:50:58 +0600

    admin middleware created

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,17 +4,27 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
     /**
-     * Handle an incoming request.
+     * Handle an incoming request and check for admin access.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * This middleware checks if the authenticated user has the 'admin' role.
+     * If the user is an admin, the request is allowed to proceed; otherwise,
+     * a 403 Forbidden response is returned.
+     *
+     * @param  \Illuminate\Http\Request  $request  The incoming request instance.
+     * @param  \Closure  $next  The next middleware to call.
+     * @return \Symfony\Component\HttpFoundation\Response  The response instance (either the next response or a 403 error).
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role == 'admin') {
+            return $next($request);
+        }
+        return abort(403, 'Forbidden');
     }
 }
